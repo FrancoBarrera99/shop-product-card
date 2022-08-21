0fffd5badbf25047053cbd23997a34c137232c51
FrancoBarrera99 <56893886+FrancoBarrera99@users.noreply.github.com>
2022-08-21 20:37:12 -0300

    Card 3D Effect

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -15,6 +15,48 @@ const ProductCard = () => {
   const color = useSelector((state) => state.color.value)
   const dispatch = useDispatch()
 
+  //movement animation
+  const container = document.querySelector('.container')
+  const card = document.querySelector('.card')
+
+  const texts = document.querySelector('.texts')
+  const colors = document.querySelector('.colors')
+  const purchase = document.querySelector('.purchase_btn')
+  const watch = document.querySelector('.wrapper img')
+
+
+  container?.addEventListener('mousemove', (e) => {
+
+    let xAxis = ( window.innerWidth / 2 - e.pageX ) / 25
+    let yAxis = ( window.innerHeight / 2 - e.pageY ) / 25
+
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+
+  })
+
+  //Animate in
+  container?.addEventListener('mouseenter', (e) => {
+
+    texts.style.transform = 'translateZ(75px)'
+    colors.style.transform = 'translateZ(75px)'
+    purchase.style.transform = 'translateZ(100px)'
+    watch.style.transform = 'translateZ(100px) rotateZ(20deg)'
+
+    card.style.transitionDuration = '0s'
+  })
+
+  //Animate out
+  container?.addEventListener('mouseleave', (e) => {
+    card.style.transition = null
+    card.style.transform = null
+
+    texts.style.transform = null
+    colors.style.transform = null
+    purchase.style.transform = null
+    watch.style.transform = null
+  })
+
+
   return (
     <div className='container'>
       <div className='card'>
